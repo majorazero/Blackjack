20954aa6bb6925957b315e27f0c97a37006112b3
majorazero <daniel.cw.hsu@gmail.com>
2017-04-10 22:35:22 -0700

    First Big Update
    - Made new Array() -> []
    - Added [0] after hand[i] so the functions will actually read the
    value. For some reason its creating an array of an array of an
    individual object. Not sure how to fix.
    - Fixed the deck.length +1 sometimes attempting to splice objects at
    impossible lengths. My bad.

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let suit = ['Spades','Diamonds','Hearts','Clubs'];
 let value = ['A','2','3','4',
             '5','6','7','8',
             '9','10','J','Q','K'];
-let deck = new Array();
+let deck = [];
 
 function createDeck(){
-  deck = new Array(); //reset pre-existing deck.
+  deck = []; //reset pre-existing deck.
   for(let i = 0; i < value.length; i++){ //goes through 1-K
     for(let j = 0; j <suit.length; j++){ //goes through spades through clubs
       let val = parseInt(value[i]); //assuming its not a letter it'll be what the number value is
@@ -29,11 +29,11 @@ function valueOfHand(hand){ //find the value of a player's Hand based on an arra
   let value = 0;
   let aAmount = 0;
   for (let i = 0; i < hand.length; i++){
-    value += hand[i].Val; //acrue total value of player's hand
-    if (hand[i].Value === 'A'){ // if the current value is an Ace, we'll increment the ace counter.
+    value += hand[i][0].Val; //acrue total value of player's hand
+    if (hand[i][0].Value === 'A'){ // if the current value is an Ace, we'll increment the ace counter.
       aAmount++;
     }
-    if (hand[i].Value === 'A' && aAmount != 0){  //There can technically only be a max of 1 "11" Ace in a hand at any one time
+    if (hand[i][0].Value === 'A' && aAmount != 0){  //There can technically only be a max of 1 "11" Ace in a hand at any one time
       value -= 10; //Subsequent aces are just "1"
     }
   }
@@ -46,7 +46,7 @@ function valueOfHand(hand){ //find the value of a player's Hand based on an arra
 
 function deal(hand,numCard){ //will deal a random card from the existant deck, no need for shuffle
   for(let i =0; i<numCard; i++){ //deals numCard amount of cards to hand
-    let randPos = Math.floor(Math.random()*(deck.length+1)); // every loop will regenerate a new random position value that takes into account the new deck length
+    let randPos = Math.floor(Math.random()*(deck.length)); // every loop will regenerate a new random position value that takes into account the new deck length
     hand.push(deck.splice(randPos,1)); //removes item at the random position and pushes it to the dealt hand
   }
 }
